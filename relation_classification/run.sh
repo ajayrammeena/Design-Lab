8db9a64795dc59a622e6364301bd71ad2c2df93f
ajayrammeena <55804608+ajayrammeena@users.noreply.github.com>
2023-05-01 14:09:46 +0530

    Add files via upload

---
+++ relation_classification/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+model_save_dir=model/
+preds_save_dir=preds/
+cache_dir=../.cache/
+
+for model_name in matscibert scibert bert; do
+    echo $model_name
+    python -u relation_classification.py --model_name $model_name --model_save_dir $model_save_dir --preds_save_dir $preds_save_dir --cache_dir $cache_dir
+done
